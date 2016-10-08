0978000d9b4bd48b9a8f37607ca9fd9ea51a6ebf
Deepika Anand <deepika@guest-wireless-upc-1603-10-120-051-229.usc.edu>
2016-10-08 15:10:40 -0700

    Commit 2 for Oct 8

---
+++ LeetCodePractice/Valid Parentheses.py
@@ -0,0 +1,49 @@
+
+class Solution:
+
+    def _isOpening(self, bracket):
+        #print ord(']'), ord('}'), ord(')')
+        if (ord(bracket) in [ord('['), ord('{'), ord('(')]):
+            return True
+        return False
+
+    def _isClosing(self, bracket):
+        #print ord(']'), ord('}'), ord(')')
+        if (ord(bracket) in [ord(']'), ord('}'), ord(')')]):
+            return True
+        return False
+
+    def _isSame(self, openbracket, bracket2):
+        if ( openbracket == '(' and bracket2 == ')'):
+            return True
+        if (openbracket == '[' and bracket2 == ']'):
+            return True
+        if (openbracket == '{' and bracket2 == '}'):
+            return True
+        return False
+
+    def isValid(self, s):
+        stack = []
+
+        for bracket in s:
+            if ( self._isClosing(bracket) and len(stack) == 0):
+                return False
+            if ( self._isOpening(bracket)):
+                stack.append(bracket)
+            else:
+                openbracket = stack.pop(-1)
+                if not( self._isSame(openbracket, bracket)):
+                    return False
+                else:
+                    continue
+        return True if (len(stack) == 0) else False
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    s = "()[]{}"
+    print sol.isValid(s)
+    s = "(]"
+    print sol.isValid(s)
+    s = "([)]"
+    print sol.isValid(s)
