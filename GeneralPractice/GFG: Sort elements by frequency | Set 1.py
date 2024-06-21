7b7f4b6f3610230fe0d8d3b283e3729dfdea4640
Deepika Anand <deepika_087@yahoo.com>
2024-06-21 13:27:44 -0700

    trial commit

--- GeneralPractice/GFG: Sort elements by frequency | Set 1.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'deepika'
-
-#ToDo: use heapification + hashmap tp keep track of nodes
-class TreeNode:
-    def __init__(self, data):
-        self.freq = 1
-        self.data = data
-        self.left = None
-        self.right = None
